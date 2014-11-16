7c1645228acd731622fcff77f47645d6d897c7e6
David Macek <david.macek.0@gmail.com>
2014-11-16 15:18:22 +0100

    ENH: Now works with a separate config file to avoid breaking pacman.conf

--- pacrepos.py
+++ pacrepos.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 
-## Issues:
-## - strips comments and whoknows what else from pacman.conf
-
 import configparser
 import argparse
 import shutil
 
 class PacConfParser(configparser.ConfigParser):
-	def __init__(self, *args, **kwargs):
-		kwargs['allow_no_value'] = True
-		configparser.ConfigParser.__init__(self, *args, **kwargs)
-
 	def optionxform(self, option):
 		return option
 
 parser = argparse.ArgumentParser(description = 'Manage pacman repositories.')
 group = parser.add_mutually_exclusive_group(required = True)
-group.add_argument('-A', '--add', action = 'store_true', help = 'Add the repository.')
-group.add_argument('-R', '--remove', action = 'store_true', help = 'Remove the repository')
-parser.add_argument('name', help = 'The repository identifier.')
-parser.add_argument('--url', help = 'The repository URL.')
+group.add_argument('-I', '--install', action = 'store_true', help = 'install into pacman.conf')
+group.add_argument('-A', '--add', action = 'store_true', help = 'add the repository NAME with URL')
+group.add_argument('-R', '--remove', action = 'store_true', help = 'remove the repository NAME')
+parser.add_argument('--name', help = 'the repository identifier')
+parser.add_argument('--url', help = 'the repository server')
 
-CONFIG_FILE = '/etc/pacman.conf'
+PACMAN_FILE = '/etc/pacman.conf'
+CONFIG_FILE = '/etc/pacrepos.conf'
 
 args = parser.parse_args()
 if args.add and not args.url:
 	raise Exception('URL is required when adding a repository.')
+if args.add and not args.name:
+	raise Exception('Name is required when adding a repository.')
+if args.remove and not args.name:
+	raise Exception('Name is required when removing a repository.')
 
-config = PacConfParser()
-config.read(CONFIG_FILE)
+if args.install:
+	shutil.copyfile(PACMAN_FILE, '{0}.old'.format(PACMAN_FILE))
+	with open(PACMAN_FILE, 'a') as file:
+		file.write('\n')
+		file.write('Include = {0}\n'.format(CONFIG_FILE))
+	with open(CONFIG_FILE, 'a+'):
+		pass # just create the file
+else:
+	config = PacConfParser()
+	config.read(CONFIG_FILE)
 
-if args.add and args.name in config:
-	raise Exception("Repository '{0}' is already present.".format(args.name))
+	if args.add and args.name in config:
+		raise Exception("Repository '{0}' is already present.".format(args.name))
 
-if args.remove and args.name not in config:
-	raise Exception("Repository '{0}' is not present.".format(args.name))
+	if args.remove and args.name not in config:
+		raise Exception("Repository '{0}' is not present.".format(args.name))
 
-if args.add:
-	config[args.name] = { 'Server': args.url, 'SigLevel': 'Optional' }
+	if args.add:
+		config[args.name] = { 'Server': args.url, 'SigLevel': 'Optional' }
 
-if args.remove:
-	del config[args.name]
+	if args.remove:
+		del config[args.name]
 
-shutil.copyfile(CONFIG_FILE, CONFIG_FILE + '.old')
-with open(CONFIG_FILE, 'w') as file:
-	config.write(file)
+	with open(CONFIG_FILE, 'w') as file:
+		config.write(file)
